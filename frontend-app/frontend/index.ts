cc648b83401747e3eefa8c9e365033065fa26d81
awelsh <antonywelsh.dev@gmail.com>
2020-04-06 14:47:15 +0100

    Frontend pulumi code

--- frontend-app/frontend/index.ts
+++ frontend-app/frontend/index.ts
@@ -1,9 +1,44 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const env = pulumi.getStack().split('.')[1];
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const siteBucket = new aws.s3.Bucket(
+  `pulumi-blog-post-${env}`,
+  {
+    bucket: `pulumi-blog-post-${env}`,
+    acl: "public-read",
+    website: {
+      indexDocument: "index.html",
+      errorDocument: "index.html"
+    }
+  }, { deleteBeforeReplace: true }
+);
+
+function publicReadPolicyForBucket(bucketName: string) {
+  return JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [{
+          Effect: "Allow",
+          Principal: "*",
+          Action: [
+              "s3:GetObject"
+          ],
+          Resource: [
+              `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
+          ]
+      }]
+  });
+}
+
+// Set the access policy for the bucket so all objects are readable
+const bucketPolicy = new aws.s3.BucketPolicy(
+  "bucketPolicy", 
+  {
+    bucket: siteBucket.bucket,
+    policy: siteBucket.bucket.apply(publicReadPolicyForBucket)
+  }
+);
+
+export const bucketName = siteBucket.bucket;
+export const websiteUrl = siteBucket.websiteEndpoint;
